f957d93ae406cc323198c23185c8b0d02882a1de
justinliow <justinliow@gmail.com>
2015-12-23 23:31:00 -0600

    dd

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 		public static void main(String []args){
 		System.out.println("Hello World");
 		System.out.println("Using this program to learn how to use Git");
+		//  dgtszgdxs
 		}
 	
 
